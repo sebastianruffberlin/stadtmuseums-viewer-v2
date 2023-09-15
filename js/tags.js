f5dfd7ac66824dfa89baa3034213ff696d1cbd79
Christopher Pietsch <cpietsch@gmail.com>
2023-09-15 08:48:31 +0200

    filter

--- js/tags.js
+++ js/tags.js
@@ -1,11 +1,10 @@
 function Tags() {
 
-  var container;
+  var containerVorbesitzerin;
 
   var fontsize = d3.scale.linear().range([11, 23])
 
-  var filter = { emigrationverfolgung: [], vorbesitzerin: [] };
-  var kaufer = [];
+  var filter = { emigrationverfolgung: [], vorbesitzerin: [], alteanonymemoderne: [] };
   var lock = false
 
   function addOrRemove(array, value) {
@@ -21,25 +20,59 @@ function Tags() {
 
   function tags() { }
 
+  tags.updateFilter = function updateFilter() {
+    alteanonymemoderneData = d3.nest()
+      .key(function (d) { return d.alteanonymemoderne; })
+      .entries(data.filter(function (d) { return d.active; }));
+
+    alteanonymemoderneContainer = d3.select(".alteanonymemoderne .items");
+    var selection = alteanonymemoderneContainer
+      .selectAll(".item")
+      .data(alteanonymemoderneData, function (d) { return d.key; });
+
+    selection
+      .enter()
+      .append("div")
+      .classed("item", true)
+      .text(function (d) {
+        return d.key;
+      })
+      .on("click", function (d) {
+        lock = true;
+        filter.alteanonymemoderne = addOrRemove(filter.alteanonymemoderne, d.key);
+        tags.filter();
+        tags.update();
+        lock = false;
+      });
+
+    selection.exit()
+      .classed("active", false)
+      .classed("hide", true)
+
+    selection.classed("active", function (d) {
+      return filter.alteanonymemoderne.indexOf(d.key) > -1;
+    }).classed("hide", false)
+  }
+
   tags.init = function (_data, config) {
     // console.log("init tags", _data, config)
     data = _data;
 
-    container = d3.select(".verkaufer");
+    tags.updateFilter()
 
-    kaufer = d3.nest()
+    vorbesitzerinData = d3.nest()
       .key(function (d) { return d.vorbesitzerin; })
       .entries(data)
       .sort(function (a, b) {
         return b.values.length - a.values.length;
       })
-    // console.log(kaufer)
 
-    fontsize.domain(d3.extent(kaufer, function (d) { return d.values.length; }))
+    fontsize.domain(d3.extent(vorbesitzerinData, function (d) { return d.values.length; }))
 
-    container.select(".list")
+    containerVorbesitzerin = d3.select(".verkaufer .list");
+    containerVorbesitzerin
       .selectAll(".item")
-      .data(kaufer)
+      .data(vorbesitzerinData, function (d) { return d.key; })
       .enter()
       .append("div")
       .classed("item", true)
@@ -67,20 +100,23 @@ function Tags() {
         tags.filter();
       })
 
-    container.select("#kauferemi")
-      .on("change", function () {
-        var isChecked = d3.select(this).property("checked");
-        // filter.emigrationverfolgung = isChecked ? "JA" : false;
-        tags.filter();
-      })
+    // container.select("#kauferemi")
+    //   .on("change", function () {
+    //     var isChecked = d3.select(this).property("checked");
+    //     // filter.emigrationverfolgung = isChecked ? "JA" : false;
+    //     tags.filter();
+    //   })
   }
 
+
   tags.update = function () {
-    container.select(".list")
+    containerVorbesitzerin
       .selectAll(".item")
       .classed("active", function (d) {
         return filter.vorbesitzerin.indexOf(d.key) > -1;
       })
+
+    tags.updateFilter();
   }
 
   tags.filter = function (highlight) {
