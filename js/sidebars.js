0465e9b139ddb7bfa575a3562af65f182dacf2d3
Christopher Pietsch <cpietsch@gmail.com>
2023-09-16 15:56:13 +0200

    added html sidebar type

--- js/sidebars.js
+++ js/sidebars.js
@@ -1,54 +1,57 @@
 var detailVue = new Vue({
-      el: '#detail',
-      data: {
-        item: null,
-        structure: null,
-        page: 0,
-        id: null
-      },
-      methods: {
-        displayPage: function(page){
-          canvas.changePage(this.id, page)
-        },
-        hasData: function(entry){
-          return this.getContent(entry) !== ''
-        },
-        getContent: function(entry) {
-          if(entry.type === 'text') {
-            return this.item[entry.source]
-          }
-          if(entry.type === 'array') {
-            return this.item[entry.source].join(', ')
-          }
-          if(entry.type === 'keywords') {
-            return this.item[entry.source].join(', ')
-          }
-          if(entry.type === 'markdown') {
-            return marked(this.item[entry.source], { breaks: true})
-          }
-          if(entry.type === 'function') {
-            const column = this.item
-            const func = entry.source
-            try {
-              return eval(func)
-            } catch (e) {
-              return 'Error'
-            }
-          }
+  el: '#detail',
+  data: {
+    item: null,
+    structure: null,
+    page: 0,
+    id: null
+  },
+  methods: {
+    displayPage: function (page) {
+      canvas.changePage(this.id, page)
+    },
+    hasData: function (entry) {
+      return this.getContent(entry) !== ''
+    },
+    getContent: function (entry) {
+      if (entry.type === 'text') {
+        return this.item[entry.source]
+      }
+      if (entry.type === 'array') {
+        return this.item[entry.source].join(', ')
+      }
+      if (entry.type === 'keywords') {
+        return this.item[entry.source].join(', ')
+      }
+      if (entry.type === 'html') {
+        return this.item[entry.source]
+      }
+      if (entry.type === 'markdown') {
+        return marked(this.item[entry.source], { breaks: true })
+      }
+      if (entry.type === 'function') {
+        const column = this.item
+        const func = entry.source
+        try {
+          return eval(func)
+        } catch (e) {
+          return 'Error'
         }
       }
-    })
-  window.detailVue = detailVue;
+    }
+  }
+})
+window.detailVue = detailVue;
 
-  var infoVue = new Vue({
-      el: '#infobar',
-      data: {
-        info: ""
-      },
-      methods: {
-        marked: function(input) {
-          return marked(input);
-        }
-      }
-    })
-  window.infoVue = infoVue;
+var infoVue = new Vue({
+  el: '#infobar',
+  data: {
+    info: ""
+  },
+  methods: {
+    marked: function (input) {
+      return marked(input);
+    }
+  }
+})
+window.infoVue = infoVue;
